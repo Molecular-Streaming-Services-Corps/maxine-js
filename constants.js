c84c50cdb1056cc8c08df853f9724a7ed11b9931
Jade O'Neill <jade@molecularreality.com>
2024-03-15 17:00:59 +1300

    Add some very useful constants myself. Most of them were in game_object.py

--- constants.js
+++ constants.js
@@ -1,2 +1,18 @@
-var worldWidth = 1800;
-var worldHeight = 900;
+const worldWidth = 1800;
+const worldHeight = 900;
+const worldCenter = [worldWidth / 2, worldHeight / 2];
+
+const torusThickness = 75;
+
+const maxineStart = [worldCenter[0] + 200, worldCenter[1]];
+
+const torusOuterWidth = worldWidth;
+const torusOuterHeight = worldHeight;
+
+const torusInnerWidth = torusOuterWidth - torusThickness * 2;
+const torusInnerHeight = torusOuterHeight - torusThickness * 2;
+const torusInnerRadius = Math.min(torusInnerWidth, torusInnerHeight) / 2;
+
+const ringWidth = torusOuterWidth - 66
+const ringHeight = torusOuterHeight - 66
+const ringRadius = Math.min(ringWidth, ringHeight) / 2
